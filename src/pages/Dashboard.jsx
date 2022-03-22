853de0ef27c72ceac957558b637aba0fe6492dbc
Agacy junior <juniormartinssom@gmail.com>
2022-03-22 10:11:48 -0300

    Dashboard finish

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,249 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import Chart from "react-apexcharts";
+
+import { useSelector } from "react-redux";
+
+import StatusCard from "../components/status-card/StatusCard";
+
+import Table from "../components/table/Table";
+
+import Badge from "../components/badge/Badge";
+
+import statusCards from "../assets/JsonData/status-card-data.json";
+
+const chartOptions = {
+  series: [
+    {
+      name: "Clientes Online",
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60],
+    },
+    {
+      name: "Clientes da loja",
+      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10],
+    },
+  ],
+  options: {
+    color: ["#6ab04c", "#2980b9"],
+    chart: {
+      background: "transparent",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Fev",
+        "Mar",
+        "Abr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Set",
+        "Out",
+        "Nov",
+        "Dez",
+      ],
+    },
+    legend: {
+      position: "top",
+    },
+    grid: {
+      show: false,
+    },
+  },
+};
+
+const topCustomers = {
+  head: ["Usuário", "Pedidos Totais", "Gasto Total"],
+  body: [
+    {
+      username: "john doe",
+      order: "490",
+      price: "R$15.870,00",
+    },
+    {
+      username: "frank iva",
+      order: "250",
+      price: "R$12.251,00",
+    },
+    {
+      username: "anthony baker",
+      order: "120",
+      price: "R$10.840,00",
+    },
+    {
+      username: "frank iva",
+      order: "110",
+      price: "R$9.251,00",
+    },
+    {
+      username: "anthony baker",
+      order: "80",
+      price: "R$8.840,00",
+    },
+  ],
+};
+
+const renderCusomerHead = (item, index) => <th key={index}>{item}</th>;
+
+const renderCusomerBody = (item, index) => (
+  <tr key={index}>
+    <td>{item.username}</td>
+    <td>{item.order}</td>
+    <td>{item.price}</td>
+  </tr>
+);
+
+const latestOrders = {
+  header: ["ID do pedido", "usuário", "preço total", "data", "status"],
+  body: [
+    {
+      id: "#OD1711",
+      user: "john doe",
+      date: "17 Jun 2021",
+      price: "RR$900",
+      status: "Enviado",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "R$400",
+      status: "Pagado",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "R$200",
+      status: "Pendente",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "R$400",
+      status: "Pagado",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "R$200",
+      status: "Reembolso",
+    },
+  ],
+};
+
+const orderStatus = {
+  Enviado: "primary",
+  Pendente: "warning",
+  Pagado: "success",
+  Reembolso: "danger",
+};
+
+const renderOrderHead = (item, index) => <th key={index}>{item}</th>;
+
+const renderOrderBody = (item, index) => (
+  <tr key={index}>
+    <td>{item.id}</td>
+    <td>{item.user}</td>
+    <td>{item.price}</td>
+    <td>{item.date}</td>
+    <td>
+      <Badge type={orderStatus[item.status]} content={item.status} />
+    </td>
+  </tr>
+);
+
+const Dashboard = () => {
+  const themeReducer = useSelector((state) => state.ThemeReducer.mode);
+
+  return (
+    <div>
+      <h2 className="page-header">Painel</h2>
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            {statusCards.map((item, index) => (
+              <div className="col-6" key={index}>
+                <StatusCard
+                  icon={item.icon}
+                  count={item.count}
+                  title={item.title}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="card full-height">
+            {/* chart */}
+            <Chart
+              options={
+                themeReducer === "theme-mode-dark"
+                  ? {
+                      ...chartOptions.options,
+                      theme: { mode: "dark" },
+                    }
+                  : {
+                      ...chartOptions.options,
+                      theme: { mode: "light" },
+                    }
+              }
+              series={chartOptions.series}
+              type="line"
+              height="100%"
+            />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card">
+            <div className="card__header">
+              <h3>Principais Clientes</h3>
+            </div>
+            <div className="card__body">
+              <Table
+                headData={topCustomers.head}
+                renderHead={(item, index) => renderCusomerHead(item, index)}
+                bodyData={topCustomers.body}
+                renderBody={(item, index) => renderCusomerBody(item, index)}
+              />
+            </div>
+            <div className="card__footer">
+              <Link to="/">Ver tudo</Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-8">
+          <div className="card">
+            <div className="card__header">
+              <h3>Últimos pedidos</h3>
+            </div>
+            <div className="card__body">
+              <Table
+                headData={latestOrders.header}
+                renderHead={(item, index) => renderOrderHead(item, index)}
+                bodyData={latestOrders.body}
+                renderBody={(item, index) => renderOrderBody(item, index)}
+              />
+            </div>
+            <div className="card__footer">
+              <Link to="/">Ver tudo</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
